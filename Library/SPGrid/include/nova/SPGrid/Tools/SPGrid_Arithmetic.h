1973cbfb3af4af71c2226401695f4145c80b78a9
Mridul Aanjaneya <mridul.aanjaneya@rutgers.edu>
2018-10-03 22:29:15 -0400

    add SPGrid masked add

---
+++ Library/SPGrid/include/nova/SPGrid/Tools/SPGrid_Arithmetic.h
@@ -0,0 +1,43 @@
+//!#####################################################################
+//! \file SPGrid_Arithmetic.h
+//!#####################################################################
+// Various classes for SPGrid arithmetic.
+//######################################################################
+#ifndef __SPGrid_Arithmetic__
+#define __SPGrid_Arithmetic__
+
+#include <nova/SPGrid/Core/SPGrid_Allocator.h>
+#include <nova/SPGrid/Tools/SPGrid_Threading_Helper.h>
+
+namespace SPGrid{
+template<class Struct_type,class T,int d>
+class Masked_Add
+{
+    using Flags_type            = typename Struct_type::Flags_type;
+    using Allocator_type        = SPGrid_Allocator<Struct_type,d>;
+    using Flag_array_mask       = typename Allocator_type::template Array_mask<unsigned>;
+
+  public:
+    Masked_Add(Allocator_type& allocator,const std::pair<const uint64_t*,unsigned>& blocks,T Struct_type::* source1_channel,
+               T Struct_type::* source2_channel,T Struct_type::* destination_channel,const unsigned mask)
+    {Run(allocator,blocks,source1_channel,source2_channel,destination_channel,mask);}
+
+    void Run(Allocator_type& allocator,const std::pair<const unsigned long*,unsigned>& blocks,T Struct_type::* source1_channel,
+             T Struct_type::* source2_channel,T Struct_type::* destination_channel,const unsigned mask) const
+    {
+        auto source1=allocator.template Get_Const_Array<Struct_type,T>(source1_channel);
+        auto source2=allocator.template Get_Const_Array<Struct_type,T>(source2_channel);
+        auto destination=allocator.template Get_Array<Struct_type,T>(destination_channel);
+        auto flags=allocator.template Get_Const_Array<Struct_type,unsigned>(&Struct_type::flags);
+
+        auto masked_add=[&](uint64_t offset)
+        {
+            for(int e=0;e<Flag_array_mask::elements_per_block;++e,offset+=sizeof(Flags_type))
+                if(flags(offset)&mask) destination(offset)=source1(offset)+source2(offset);
+        };
+
+        SPGrid_Computations::Run_Parallel_Blocks(blocks,masked_add);
+    }
+};
+}
+#endif
